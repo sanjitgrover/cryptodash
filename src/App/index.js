f7cbd5dd97cd3da904ba7136b2c1b6e851a9e2db
sanjitgrover <sanjit.grover@gmail.com>
2019-08-02 12:43:04 +0530

    restructure files

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import './App.css';
-import AppLayout from './AppLayout';
-import AppBar from './AppBar'
-import AppProvider from './AppProvider';
+
 
 const MyButton = styled.div`
 color: white;
@@ -11,13 +9,10 @@ color: white;
 
 function App() {
   return (
-    <AppProvider>
-    <AppLayout>
-      <AppBar>
+  
       <MyButton>Hello World</MyButton>
-      </AppBar>
-    </AppLayout>
-    </AppProvider>
+     
+   
   );
 }
 
