d4c1958bee9a082c9c1a2cb003f25e89901cd677
sanjitgrover <sanjit.grover@gmail.com>
2019-08-09 15:23:56 +0530

    favorites

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import _ from 'lodash';
-const cc = require("cryptocompare")
+import _ from "lodash";
+const cc = require("cryptocompare");
 export const AppContext = React.createContext();
 
 const MAX_FAVORITES = 10;
@@ -17,7 +17,8 @@ export class AppProvider extends Component {
       removeCoin: this.removeCoin,
       isInFavorites: this.isInFavorites,
       confirmFavorites: this.confirmFavorites,
-      setFilteredCoins: this.setFilteredCoins
+      setFilteredCoins: this.setFilteredCoins,
+      setCurrentFavorite: this.setCurrentFavorite
     };
   }
 
@@ -30,69 +31,82 @@ export class AppProvider extends Component {
     this.setState({ coinList: coinList.Data });
   };
 
-  fetchPrices = async()=>{
-    if(this.state.firstVisit){
+  fetchPrices = async () => {
+    if (this.state.firstVisit) {
       return;
     }
     let prices = await this.prices();
-    prices=prices.filter(price=>Object.keys(price).length);
+    prices = prices.filter(price => Object.keys(price).length);
     //console.log("prices", prices);
-    this.setState({prices});
-  }
+    this.setState({ prices });
+  };
 
-  prices=async()=>{
+  prices = async () => {
     let returnData = [];
-    for(let i=0;i<this.state.favorites.length; i++){
-      try{
-        let priceData = await cc.priceFull(this.state.favorites[i], 'USD');
+    for (let i = 0; i < this.state.favorites.length; i++) {
+      try {
+        let priceData = await cc.priceFull(this.state.favorites[i], "USD");
         returnData.push(priceData);
-      }catch(e){
-        console.warn('Fetch Price Error', e);
+      } catch (e) {
+        console.warn("Fetch Price Error", e);
       }
     }
     return returnData;
-  }
+  };
 
-  addCoin=key=>{
-    let {favorites} = this.state;
-    if(favorites.length < MAX_FAVORITES){
-    favorites.push(key);
-    this.setState({favorites});
+  addCoin = key => {
+    let { favorites } = this.state;
+    if (favorites.length < MAX_FAVORITES) {
+      favorites.push(key);
+      this.setState({ favorites });
     }
-  }
+  };
 
-  removeCoin =key=>{
-    let {favorites} = this.state;
-    this.setState({favorites: _.pull(favorites, key)});
-  }
+  removeCoin = key => {
+    let { favorites } = this.state;
+    this.setState({ favorites: _.pull(favorites, key) });
+  };
 
-  isInFavorites = key=> _.includes(this.state.favorites , key)
+  isInFavorites = key => _.includes(this.state.favorites, key);
 
   confirmFavorites = () => {
-    this.setState({ firstVisit: false, page: "dashboard" },
-    ()=>{
-      this.fetchPrices();
-    }
+    let currentFavorite = this.state.favorites[0];
+    this.setState(
+      { firstVisit: false, page: "dashboard", currentFavorite },
+      () => {
+        this.fetchPrices();
+      }
     );
     localStorage.setItem(
       "cryptodash",
       JSON.stringify({
-        favorites: this.state.favorites
+        favorites: this.state.favorites,
+        currentFavorite
       })
     );
   };
+
+  setCurrentFavorite=sym=>{
+    this.setState({
+      currentFavorite: sym
+    })
+    localStorage.setItem('cryptodash', JSON.stringify({
+      ...JSON.parse(localStorage.getItem('cryptodash')),
+      currentFavorite: sym
+    }))
+  }
   savedSettings() {
     let cryptoDashData = JSON.parse(localStorage.getItem("cryptodash"));
     if (!cryptoDashData) {
       return { page: "settings", firstVisit: true };
     }
-    let {favorites} = cryptoDashData; 
-    return {favorites};
+    let { favorites, currentFavorite } = cryptoDashData;
+    return { favorites, currentFavorite };
   }
 
   setPage = page => this.setState({ page });
 
-  setFilteredCoins =(filteredCoins)=>this.setState({filteredCoins});
+  setFilteredCoins = filteredCoins => this.setState({ filteredCoins });
 
   render() {
     return (
--- src/Dashboard/PriceTile.js
+++ src/Dashboard/PriceTile.js
@@ -1,33 +1,98 @@
-import React from 'react'
-import styled, {css} from 'styled-components'
-import {SelectableTile} from "../Shared/Tile"
-import { CoinHeaderGridStyled } from '../Settings/CoinHeaderGrid';
-import {fontSize3} from "../Shared/Styles"
+import React from "react";
+import styled, { css } from "styled-components";
+import { SelectableTile } from "../Shared/Tile";
+import { CoinHeaderGridStyled } from "../Settings/CoinHeaderGrid";
+import { fontSize3, fontSizeBig, greenBoxShadow } from "../Shared/Styles";
+import { AppContext } from "../App/AppProvider";
 
+const JustifyRight = styled.div`
+  justify-self: right;
+`;
+
+const JustifyLeft = styled.div`
+  justify-self: left;
+`;
+
+const TickerPrice = styled.div`
+  ${fontSizeBig}
+`;
+
+const ChangePct = styled.div`
+  color: green;
+  ${props =>
+    props.red &&
+    css`
+      color: red;
+    `}
+`;
 
 const PriceTileStyled = styled(SelectableTile)`
-    ${props=> props.compact && css`
-        ${fontSize3}
+  ${props =>
+    props.compact &&
+    css`
+      display: grid;
+      ${fontSize3}
+      grid-gap: 5px;
+      grid-template-columns: repeat(3, 1fr);
+      justify-items: right;
     `}
-`
-function PriceTile({sym, data}){
-    return(
-        <PriceTileStyled>
-        <CoinHeaderGridStyled>
-        <div>{sym}</div>
-        <div>{data.CHANGEPCT24HOUR}</div>
-        </CoinHeaderGridStyled>
-        </PriceTileStyled>
-    );
+  ${props =>
+    props.currentFavorite &&
+    css`
+      ${greenBoxShadow}
+      pointer-events: none;
+    `}
+`;
+
+function ChangePercent({ data }) {
+  return (
+    <JustifyRight>
+      <ChangePct red={data.CHANGEPCT24HOUR < 0}>
+        {Number(data.CHANGEPCT24HOUR).toFixed(4)}
+      </ChangePct>
+    </JustifyRight>
+  );
 }
 
-export default function ({price, index}) {
-    let sym=Object.keys(price)[0];
-    let data=price[sym]["USD"];
-    //console.log("recieved price", price);
-    return (
-        <PriceTile 
-           sym={sym} data={data}>
-        </PriceTile>
-    )
+function PriceTileCompact({ sym, data, currentFavorite, setCurrentFavorite }) {
+  return (
+    <PriceTileStyled onClick={setCurrentFavorite} compact currentFavorite={currentFavorite}>
+      <JustifyLeft>{sym}</JustifyLeft>
+      <ChangePercent data={data} />
+
+      <div>${Number(data.PRICE).toFixed(4)}</div>
+    </PriceTileStyled>
+  );
+}
+
+function PriceTile({ sym, data, currentFavorite, setCurrentFavorite }) {
+  return (
+    <PriceTileStyled  onClick={setCurrentFavorite}  currentFavorite={currentFavorite}>
+      <CoinHeaderGridStyled>
+        <div>{sym}</div>
+        <ChangePercent data={data} />
+      </CoinHeaderGridStyled>
+      <TickerPrice>${Number(data.PRICE).toFixed(4)}</TickerPrice>
+    </PriceTileStyled>
+  );
+}
+
+export default function({ price, index }) {
+  let sym = Object.keys(price)[0];
+  let data = price[sym]["USD"];
+  let TileClass = index < 5 ? PriceTile : PriceTileCompact;
+  return (
+    <AppContext.Consumer>
+      {({ currentFavorite, setCurrentFavorite }) => {
+        return (
+          <TileClass
+            sym={sym}
+            data={data}
+            currentFavorite={currentFavorite === sym}
+            setCurrentFavorite={()=>setCurrentFavorite(sym)}
+          />
+        );
+      }}
+    </AppContext.Consumer>
+  );
 }
